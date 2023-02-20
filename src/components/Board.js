bbeccd5f8f9c0233379b93404201e61e9d8f9ef2
Florian Serger <floserger@t-online.de>
2023-02-20 18:38:38 +0100

    Project Setup

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+export default function Board() {
+
+    const BOARD_SIZE = 15;
+
+    return (
+        <div>
+            Board
+        </div>
+    )
+}
