818d27df3c633b44836c31ddb19bbb8f26283b99
Florian Serger <floserger@t-online.de>
2023-02-20 19:17:19 +0100

    setup 2d board

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,9 +4,21 @@ export default function Board() {
 
     const BOARD_SIZE = 15;
 
+    const [score, setScore] = useState(0);
+    const [board, setBoard] = useState(
+        new Array(BOARD_SIZE).fill(0).map(row => new Array(BOARD_SIZE).fill(0))
+    );
+
     return (
         <div>
-            Board
+            <h2 className="score">Score: {score}</h2>
+            <div className="board">
+                {board.map((row) => (
+                    row.map((cell, cellID) => (
+                        <div className="cell" key={cellID}></div>
+                    ))
+                ))}
+            </div>
         </div>
     )
 }
